3ead1d2503fd52525a2315fe6a98e952ce5f89ec
Istiaq Ansari <ansariistiaq@gmail.com>
2019-09-23 16:28:31 +0600

    fixed e train parts, compare mild sample removal added

--- codes/dataholder.py
+++ codes/dataholder.py
@@ -2,50 +2,73 @@ import matplotlib.pyplot as plt,h5py,numpy as np
 from collections import Counter
 
 class Data():
-    def __init__(self,path,f,n):
+    def __init__(self,path,f,n,severe = False):
         self.data = h5py.File(path+f, 'r')
         self.file = f
         self.dom = n
-        self.trainX = self.data['trainX']
-        self.trainY = self.data['trainY'][:][0]
-        self.train_parts = self.data['train_parts'][0]
+        self.trainX = np.array(self.data['trainX'][:]).astype('float32')
+        self.trainY = self.data['trainY'][:][0].astype('int8')
+        self.train_parts = self.data['train_parts'][0].astype('int32')
         self.valX = None
         self.valY = None
         self.normfiles, self.abnormfiles = self.parts()
         self.domainY = [n]*self.trainY.shape[0]
+        
+        self.normal = Counter(self.trainY)[0]
+        self.abnormal = Counter(self.trainY)[1]
+        self.total = self.normal+self.abnormal
+        
         if(f[:3]=='com'):
-            self.trainY[self.trainY>0] = 1
-            self.valX = self.data['valX']
+            self.valX = self.data['valX'][:]
             self.valY = self.data['valY'][:][0]
             self.valY[self.valY>0] = 1
+            if(severe):
+                self.sevX = None
+                self.sevY = self.trainY[self.trainY<2]
+                self.sev_parts = []
+                left = int(0)
+                for x in d.train_parts:
+                    x = int(x)
+                    if(all([i==2 for i in d.trainY[left:x+left]])):
+                        if(self.sevX is None):
+                            self.sevX = self.trainX[:,left:x+left]
+                        else: 
+                            self.sevX = np.concatenate((self.sevX,self.trainX[:,left:x+left]),axis=1)
+                        
+                    else:self.sev_parts.append(x)
+                    left = x + left
+            else:
+                self.trainY[self.trainY>0] = 1
         elif(f[:3]=='pas'):
             self.trainY[self.trainY<0] = 0
         else:
             self.trainY[self.trainY<0] = 0
-        #print(Counter(self.trainY))
-        self.normal = Counter(self.trainY)[0]
-        self.abnormal = Counter(self.trainY)[1]
-        self.total = self.normal+self.abnormal
         if('fold_e' in f):
+            print(self.trainY.shape)
             nn = ab = 0
             idx = []
-            for x in self.trainY:
-                if x==0:
+            left = 0
+            parts = []
+            for x in self.train_parts:
+                if(all([(l == 0) for l in self.trainY[left:left+x]])):
                     if(nn<self.abnormal):
-                        idx.append(True)
-                        nn += 1
-                    else:idx.append(False);
+                        idx = idx + [True]*x
+                        nn = nn + x
+                        parts.append(x)
+                    else:idx = idx + [False]*x
                 else:
                     if(ab<self.abnormal):
-                        idx.append(True)
-                        ab += 1
-                    else:idx.append(False);
+                        idx = idx + [True]*x
+                        ab = ab + x
+                        parts.append(x)
+                    else:idx = idx + [False]*x
+                left = left + x
             self.trainY = self.trainY[idx]
             self.trainX = np.transpose(np.transpose(self.trainX)[idx])
-            self.train_parts = None
+            self.train_parts = parts
             self.total = nn+ab
             self.domainY = self.domainY[:self.total]
-            self.normfiles, self.abnormfiles = nn,ab
+            print(self.trainY.shape)
     def parts(self):
         y = 0
         nn = 0
@@ -55,6 +78,7 @@ class Data():
                 ab = ab + 1
             else: nn = nn +1 
             y = int(x)+y
+        #print(nn+ab,nn,ab)
         return nn,ab
     def pie(self):
         colors = ['gold','lightskyblue']
@@ -64,3 +88,5 @@ class Data():
         plt.pie(size,labels=labels,colors = colors,startangle=140,explode=explode,
                shadow=True,autopct=self.value)
         plt.title(self.dom)
+    def value(self,val):
+        return int(self.total*val/100)
